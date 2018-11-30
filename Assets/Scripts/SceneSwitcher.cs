64f1b8d18d6312a21a03877e19c30956a470fe8c
DanBomb <doctormcnaughton@gmail.com>
2018-11-30 13:18:54 -0500

    Initial commit

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	public void SceneSwitcherBlack () {
+
+		SceneManager.LoadScene (1);
+	}
+	public void SceneSwitcherGreen () {
+
+		SceneManager.LoadScene (2);
+	}
+	public void SceneSwitcherBlue () {
+
+		SceneManager.LoadScene (0);
+	}
+}
